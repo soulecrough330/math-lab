09a35aed4ffc4c64e65a36c776c23cb14d389b81
soulecrough330 <soulecrough330@gmail.com>
2025-03-11 04:50:50 +0000

    Add 6.js

---
+++ src/6.js
@@ -0,0 +1,7 @@
+const mathLab = () => {
+  // Generate a random number between 1 and 10
+  const randNum = Math.floor(Math.random() * 10) + 1;
+  
+  // Display the result
+  console.log(`Random number: ${randNum}`);
+}
