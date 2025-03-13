7154afc82c8ef0e2fe1643e4c946a3433a9ff0dc
soulecrough330 <soulecrough330@gmail.com>
2025-03-13 11:34:23 +0000

    Add 7.js

---
+++ src/7.js
@@ -0,0 +1,19 @@
+// Example 1: Simple Calculator using JavaScript
+
+const calculate = (num1, num2) => {
+let result;
+switch(operator){
+case "+":
+result = num1 + num2;
+break;
+case "-":
+result = num1 - num2;
+break;
+case "*":
+result = num1 * num2;
+break;
+case "/":
+result = num1 / num2;
+}
+return result;
+};
