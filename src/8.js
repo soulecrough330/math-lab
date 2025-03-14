afc40c0e0201ff82c98b665180dc958489fa698f
soulecrough330 <soulecrough330@gmail.com>
2025-03-14 15:56:30 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,10 @@
+// Random Math Lab Code
+
+const myRandomNumber = Math.floor(Math.random() * 10);
+console.log("Your random number is " + myRandomNumber);
+
+if (myRandomNumber % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
