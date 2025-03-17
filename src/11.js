e89d9a25a0cf4ed218dce341a6258ba508bd11da
soulecrough330 <soulecrough330@gmail.com>
2025-03-17 09:21:26 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,11 @@
+`
+console.log("Welcome to your math lab!");
+
+// Define the function to calculate the area of a rectangle
+function rectArea(width, height) {
+  return width * height;
+}
+
+// Call the function with two input values and print the result
+const result = rectArea(5, 4);
+console.log("The area of the rectangle is " + result);`
