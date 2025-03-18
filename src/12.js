f857a9f6a3df95ac6e2d05fd479327621bbac798
soulecrough330 <soulecrough330@gmail.com>
2025-03-18 03:24:40 +0000

    Add 12.js

---
+++ src/12.js
@@ -0,0 +1,21 @@
+console.log("This is an example of how you could use JavaScript to perform some basic math operations in your project.");
+
+// Addition
+let x = 5;
+let y = 3;
+console.log(x + y); // Output: 8
+
+// Subtraction
+let z = 10;
+let w = 7;
+console.log(z - w); // Output: 3
+
+// Multiplication
+let a = 4;
+let b = 2;
+console.log(a * b); // Output: 8
+
+// Division
+let c = 10;
+let d = 2;
+console.log(c / d); // Output: 5
