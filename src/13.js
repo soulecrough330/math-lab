4aebc8dfc949fadb0a968fbf7145cd6b2eb0cc3c
soulecrough330 <soulecrough330@gmail.com>
2025-03-19 21:14:17 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,12 @@
+// Random JS Code
+
+let x = Math.random() * 10;
+console.log(x);
+
+if (x > 5) {
+    console.log("The number is greater than 5");
+} else if (x < 5) {
+    console.log("The number is lesser than 5");
+} else {
+    console.log("The number is equal to 5");
+}
