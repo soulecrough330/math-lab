e22e86fc0efb109e206c20aa2d131ba8fd7f58f9
soulecrough330 <soulecrough330@gmail.com>
2025-03-24 19:20:29 +0000

    Add 17.js

---
+++ src/17.js
@@ -0,0 +1,8 @@
+// Example: This is just an example to illustrate how to use JavaScript.
+// You can replace it with your own logic or user-defined functions here.
+
+function add(a, b) {
+  return a + b;
+}
+
+add(3, 5);
