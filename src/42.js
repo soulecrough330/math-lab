bd08647865283d4455eb8dd4d3ea0658a1a0182a
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 08:13:30 +0000

    Add 42.js

---
+++ src/42.js
@@ -0,0 +1,4 @@
+// This is a JavaScript example to demonstrate basic HTML5 Canvas functionality.
+// It generates an image on the canvas that can be zoomed in or out with a click action.
+// To use this code, add the following script tag to your HTML file:
+// <canvas id="myCanvas" width="600" height="400"></canvas>
