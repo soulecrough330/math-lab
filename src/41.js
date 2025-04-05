e859417aa99afbed757c8c89f98c2f77519c7198
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 08:08:43 +0000

    Add 41.js

---
+++ src/41.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const randomNumber = getRandomInt(10); // Random integer between 0 and 9
+console.log(`Random number: ${randomNumber}`);
