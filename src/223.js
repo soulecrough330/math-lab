11ef884a4c57b329daa75564e7bd7f1756e21cbf
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 22:46:05 +0000

    Add 223.js

---
+++ src/223.js
@@ -0,0 +1,16 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+function subtractNumbers(a, b) {
+  return a - b;
+}
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+function divideNumbers(a, b) {
+  if (b === 0) throw new Error("Cannot divide by zero.");
+  return a / b;
+}
