3a06b0f83ccf0a91547721d72da1625a9988f8a9
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 08:05:11 +0000

    Add 40.js

---
+++ src/40.js
@@ -0,0 +1,9 @@
+// Example 1: Simple console.log function
+console.log("Hello, world!");
+
+// Example 2: Function to calculate the area of a rectangle
+function calculateArea(length, width) {
+    return length * width;
+}
+
+calculateArea(5, 3); // Output: 15
