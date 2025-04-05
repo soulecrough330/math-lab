7e772024ea7ff4252ac2cea1325d852f4b488de6
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 21:00:29 +0000

    Add 201.js

---
+++ src/201.js
@@ -0,0 +1,7 @@
+function sumNumbers(a, b) {
+  // This function calculates the sum of two numbers
+  const result = a + b;
+  return result;
+}
+
+// Example usage: console.log(sumNumbers(5, 3)); // Output: 8
