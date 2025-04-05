49ad1fef5752945ef3e0076d8ed57b1423904392
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 16:18:02 +0000

    Add 142.js

---
+++ src/142.js
@@ -0,0 +1,6 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage:
+console.log(getRandomInt(10)); // Outputs a random integer between 0 and 9
