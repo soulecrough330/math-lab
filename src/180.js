1217bf5a3cab7d256b844eacd4033e24f7c2d112
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 19:22:09 +0000

    Add 180.js

---
+++ src/180.js
@@ -0,0 +1,10 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+console.log(addNumbers(2, 3)); // Output: 5
+
+// Example of using an arrow function
+const multiply = (a, b) => a * b;
+
+console.log(multiply(4, 5)); // Output: 20
