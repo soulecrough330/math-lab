c7910e067b236f66171156b18b944bd83b179dcf
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 20:34:08 +0000

    Add 195.js

---
+++ src/195.js
@@ -0,0 +1,10 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const num1 = getRandomInt(10);
+console.log(num1); // Output will be a random integer between 0 and 9
+
+num2 = getRandomInt(5);
+console.log(num2); // Output will be a random integer between 0 and 4
