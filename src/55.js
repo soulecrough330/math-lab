241d43b1443af7584a516f18c6a99dfc986e5f60
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 09:17:07 +0000

    Add 55.js

---
+++ src/55.js
@@ -0,0 +1,6 @@
+function calculateArea(radius) {
+  const area = Math.PI * radius ** 2;
+  console.log("The area of the circle with radius " + radius + " is: " + area);
+}
+
+calculateArea(5); // Example usage: calculates the area of a circle with radius 5
