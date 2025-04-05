2b7e2cf26aea1e0b40da094cfda7b6c60c1d3b91
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 19:41:18 +0000

    Add 184.js

---
+++ src/184.js
@@ -0,0 +1,7 @@
+// Example JavaScript code to demonstrate variable initialization
+
+const num1 = 5;
+const num2 = "6";
+
+console.log(`The value of num1 is ${num1}.`);
+console.log(`The value of num2 is ${num2}`);
