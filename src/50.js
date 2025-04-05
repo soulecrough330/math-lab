191ceb26fa770cf803d81de896b769b4b9f6971f
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 08:51:53 +0000

    Add 50.js

---
+++ src/50.js
@@ -0,0 +1,8 @@
+// Define an array of numbers
+let numbers = [1, 2, 3, 4, 5];
+
+// Loop through each number in the array
+for (let i = 0; i < numbers.length; i++) {
+    // Print the current number
+    console.log(numbers[i]);
+}
