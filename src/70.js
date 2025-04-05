0112274fd0881ca9e47e16df4689d3e678844884
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 10:27:09 +0000

    Add 70.js

---
+++ src/70.js
@@ -0,0 +1,16 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  if (y === 0) throw new Error("Divide by zero is not allowed.");
+  return x / y;
+}
