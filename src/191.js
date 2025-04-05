edd5b69af61178e6f315ddae266f0f10979ca5b3
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 20:14:58 +0000

    Add 191.js

---
+++ src/191.js
@@ -0,0 +1,19 @@
+// Example code demonstrating a basic calculator function
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y === 0) {
+        return "Error: Division by zero";
+    }
+    return x / y;
+}
