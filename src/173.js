aeea766262c6f8dabdab1242735efbc630ff542d
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 18:45:59 +0000

    Add 173.js

---
+++ src/173.js
@@ -0,0 +1,4 @@
+// Example of generating random numbers within a specific range
+const range = 10; // Define the lower and upper bounds
+const randomNumber = Math.floor(Math.random() * (range - 1 + 1)) + 1;
+console.log(randomNumber);
