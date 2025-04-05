24d43dcd6012b582fb77e5c7c667fb1c25f92472
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 10:47:53 +0000

    Add 74.js

---
+++ src/74.js
@@ -0,0 +1,6 @@
+function calculateSquareArea(sideLength) {
+  const area = sideLength * sideLength;
+  return `The area of a square with side length ${sideLength} is: ${area}`;
+}
+
+console.log(calculateSquareArea(5));
