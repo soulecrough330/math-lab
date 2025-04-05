c87fe81d17191cce3e442130b6032b9549ebbd52
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 21:22:12 +0000

    Add 205.js

---
+++ src/205.js
@@ -0,0 +1,15 @@
+function findMax(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+function sumOfSquares(a, b) {
+  return a * a + b * b;
+}
+
+function multiplyByTwo(x) {
+  return x * 2;
+}
