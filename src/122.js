c3ecf6fb48b32f746b06f7daa3b2c42aeddf603f
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 14:41:54 +0000

    Add 122.js

---
+++ src/122.js
@@ -0,0 +1,12 @@
+// This is an example of how to create a simple JavaScript file.
+// It can be used in various projects to define functions or variables.
+
+/**
+ * Example function for demonstration purposes only.
+ * You can replace this with your own implementation.
+ */
+function calculateArea(radius) {
+  return Math.PI * radius ** 2;
+}
+
+console.log("The area of the circle is:", calculateArea(5));
