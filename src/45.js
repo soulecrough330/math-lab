80dbae0d658a84d6ae3482ebc3a50a0b353548a7
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 08:26:39 +0000

    Add 45.js

---
+++ src/45.js
@@ -0,0 +1,6 @@
+function createShape(shape, options) {
+  // Implementation of creating shape logic using options
+  console.log(`Creating ${shape} with options:`, options);
+}
+
+createShape("triangle", { side1: "50px", side2: "70px" });
