046c2f863e819ae9bda5200f7d2050847f230b32
soulecrough330 <soulecrough330@gmail.com>
2025-04-05 08:41:02 +0000

    Add 48.js

---
+++ src/48.js
@@ -0,0 +1,7 @@
+function calculateArea(radius) {
+    const PI = 3.14;
+    const area = PI * radius ** 2;
+    console.log(`The area of the circle with radius ${radius} is: ${area}`);
+}
+
+calculateArea(5);
