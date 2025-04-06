05c4b4425fddef996c954948abfa83d1bce9d8a4
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 04:37:50 +0000

    Add 290.js

---
+++ src/290.js
@@ -0,0 +1,10 @@
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+console.log(sumNumbers(5, 3)); // Output: 8
+console.log(multiplyNumbers(4, 2)); // Output: 8
