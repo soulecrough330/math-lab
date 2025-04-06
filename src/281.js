80b25fb0c4b457b1d82f90fcc202d26390f25ed7
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 03:50:12 +0000

    Add 281.js

---
+++ src/281.js
@@ -0,0 +1,9 @@
+function getRandomInt(max) {
+  Math.random();
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const randomNumber = getRandomInt(10); // Generate a random integer between 0 and 9
+
+console.log(randomNumber); // Output: 4
