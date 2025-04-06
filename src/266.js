d8b65c8edf336ec5dc9861782b9686258ef994f9
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 02:29:56 +0000

    Add 266.js

---
+++ src/266.js
@@ -0,0 +1,10 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+const num1 = getRandomInt(5); // Generates a random integer between 0 and 4 (inclusive)
+console.log(num1);
+
+num2 = getRandomInt(5); // Generates a random integer between 0 and 4 (exclusive)
+console.log(num2);
