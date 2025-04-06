1b0919baea741cdd9f5978541a894eb0671fe518
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 07:55:36 +0000

    Add 326.js

---
+++ src/326.js
@@ -0,0 +1,12 @@
+// This is a simple example of an infinite loop.
+var count = 0;
+
+function countUp() {
+    while (true) {
+        console.log(count);
+        count++;
+    }
+}
+
+// Call the function to start the infinite loop
+countUp();
