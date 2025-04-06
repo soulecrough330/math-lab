990087fa3a8b98298dfba28c5d340648b157986e
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 09:46:56 +0000

    Add 347.js

---
+++ src/347.js
@@ -0,0 +1,9 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Example usage:
+
+console.log(getRandomInt(10)); // Output: 4
+
+console.log(getRandomInt(20)); // Output: 15
