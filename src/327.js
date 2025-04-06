750f92f7772d465efd97e0c60350cd861aacf7eb
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 08:03:53 +0000

    Add 327.js

---
+++ src/327.js
@@ -0,0 +1,8 @@
+// Example of a simple function to multiply two numbers using JavaScript's built-in `Math` object
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let result = multiply(4, 5);
+console.log(result); // Output: 20
