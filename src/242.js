eedc269be8d411853d23de14f1db3d5e14bf51a1
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 00:18:19 +0000

    Add 242.js

---
+++ src/242.js
@@ -0,0 +1,7 @@
+// Example of creating an array with random numbers between 0 and 9
+const randomNumbers = [Math.floor(Math.random() * 10)];
+
+// Example of sorting the created array
+randomNumbers.sort((a, b) => a - b);
+
+console.log(randomNumbers);
