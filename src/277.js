1438b7f1f74435398622b9b9a1872b45b80be97b
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 03:28:24 +0000

    Add 277.js

---
+++ src/277.js
@@ -0,0 +1,22 @@
+// Example of a simple JavaScript file with basic functions
+// ...
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) throw new Error('Cannot divide by zero');
+  return a / b;
+}
+
+// Example of an empty function
+function exampleFunction() {}
