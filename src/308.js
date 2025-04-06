5c8445d1f929660f3d14cb1126b96ac5b83764e7
soulecrough330 <soulecrough330@gmail.com>
2025-04-06 06:12:41 +0000

    Add 308.js

---
+++ src/308.js
@@ -0,0 +1,6 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+// Example usage: Generate a random number between 1 and 10
+console.log(getRandomInt(10)); // Output might be something like "6"
