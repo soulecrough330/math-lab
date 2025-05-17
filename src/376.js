46703521624528895dcac478efe867341cd66221
soulecrough330 <soulecrough330@gmail.com>
2025-05-17 03:56:14 +0000

    Add 376.js

---
+++ src/376.js
@@ -0,0 +1,11 @@
+// Example of a simple HTML file
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Math Lab</title>
+</head>
+<body>
+	<h1>Welcome to Math Lab!</h1>
+	<p>This is a simple demonstration of mathematical concepts through experiments and simulations.</p>
+</body>
+</html>
