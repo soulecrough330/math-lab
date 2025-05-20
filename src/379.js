8f95047dd8594418e4c01b65f5c158940113df1e
soulecrough330 <soulecrough330@gmail.com>
2025-05-20 05:28:04 +0000

    Add 379.js

---
+++ src/379.js
@@ -0,0 +1,7 @@
+// Define an array of numbers to sort
+const numbers = [5, 2, 8, 3, 9, 1, 6];
+
+// Sort the array using JavaScript's Array.prototype.sort method
+numbers.sort();
+
+console.log(numbers);
