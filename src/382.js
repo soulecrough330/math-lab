06b304394cb5e327da5fc607f964919ded1a155b
soulecrough330 <soulecrough330@gmail.com>
2025-05-25 09:20:10 +0000

    Add 382.js

---
+++ src/382.js
@@ -0,0 +1,40 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2 || num === 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+
+  return true;
+}
+
+function gcd(a, b) {
+  while (b !== 0) {
+    let temp = a % b;
+    a = b;
+    b = temp;
+  }
+  return a;
+}
+
+function maxMin(arr) {
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+  }
+
+  return [min, max];
+}
+
+function sumOfSquares(arr) {
+  let total = 0;
+  for (let num of arr) {
+    total += num * num;
+  }
+  return total;
+}
